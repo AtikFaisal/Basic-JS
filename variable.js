426d34ad2cb7c2b90ec6e74495590f4165844ad6
Atik Faisal <atik.faisal111684@gmail.com>
2022-04-05 10:54:40 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,16 @@
+var NAME = "Atik Faisal";
+console.log(NAME);
+
+let age = 22;
+console.log(age);
+
+// let input = prompt("What is your name?");
+// alert("Hello " + input);
+
+let address;
+address="Chapainawabganj"
+console.log(address);
+
+const city = ["Dhaka", "Rqajshahi", "Chapainawabganj"];
+city.push("Sirajganj")
+console.log(city);
