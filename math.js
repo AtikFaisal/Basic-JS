a6a86dbfc4c298c66a719b8ff4e5a494ce20c23c
Atik Faisal <atik.faisal111684@gmail.com>
2022-04-07 01:11:50 +0600

    math-and-love-calculator

---
+++ math.js
@@ -0,0 +1,7 @@
+let firstName = prompt ("What is your First Name")
+let partnerName = prompt ("What is your Partner Name")
+
+let number = Math.random() * 100;
+number = Math.floor(number);
+
+alert(firstName + " and " + partnerName + "'s love percentage is; " + number + "%")
