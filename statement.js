2666ec061650765800028139411ee7d3046589d1
Atik Faisal <atik.faisal111684@gmail.com>
2022-04-10 11:48:26 +0600

    varity-of-statement

---
+++ statement.js
@@ -0,0 +1,49 @@
+                              // BASIC IF STATEMENT
+// let number = 24;
+// if (number == 24) {
+//     console.log("Yess!!!")
+// } else {
+//     console.log("No!!!!")
+// }
+
+                             // COMPARISON OPERATOR
+// let number = 0;
+// if (number >= 0) {
+//     console.log("The Number is Positive!!!")
+// } else if (number < 0) {
+//     console.log("The Number is Nagetive!!!")
+// } else {
+//     console.log("The Number is ZERO!!!")
+// }
+
+// let number = 18;
+// if (number != 18) {
+//     console.log("You are Votar!!!")
+// } else {
+//     console.log("You are Not Votar!!!")
+// } 
+
+                            // Difference Between == And ===
+// let num = "34";
+// if (num == 34) {
+//     console.log("Yes!! It's 34")
+//     console.log(typeof num);
+// }
+                              //Logical Operator
+let name = "Faisal";
+let age = 22;
+let haveDegree = true;
+let experience = 5;
+
+// if (haveDegree == true && experience >= 2) {
+//     console.log (name + ",You are capable to work here!!");
+// } else {
+//     console.log ("Sorry! Try again leter");
+// }
+
+if (haveDegree == true || age >= 18) {
+    console.log (name + ",You are capable to work here!!");
+} else {
+    console.log ("Sorry! Try again leter");
+}
+
