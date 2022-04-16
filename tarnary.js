f2c18773bbcd0f91ec56d0c17e1b30ad69e97b49
Atik Faisal <atik.faisal111684@gmail.com>
2022-04-16 12:07:51 +0600

    tarnary

---
+++ tarnary.js
@@ -0,0 +1,11 @@
+var n = -1;
+var result;
+
+// var remainder = n % 2;
+
+// result = (remainder == 0) ? "even" : "odd" ;
+// console.log("This number is",result);
+
+
+result = (n < 0) ? "negative" : "positive" ;
+console.log("This number is",result);
