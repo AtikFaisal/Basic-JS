6a97e1a78300159f6ce3b8e527800544ed33236b
Atik Faisal <atik.faisal111684@gmail.com>
2022-04-17 09:57:32 +0600

    type-conversion

---
+++ type-conversion.js
@@ -0,0 +1,25 @@
+var str = "1000"
+var n = 10;
+
+// console.log(str * n)
+// console.log(str + n)
+// console.log(str)
+// console.log(Number(str))
+// console.log(Number.parseInt(str))
+// console.log(n.toString())
+// console.log(Number(Infinity))
+// console.log(Boolean(Infinity))
+
+// Falsy Value
+// ''
+// 0
+// null
+// undefined
+// NaN
+
+console.log(Boolean(''))
+console.log(Boolean("Atik Faisal"))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+console.log(Boolean(0))
+console.log(Boolean(121))
