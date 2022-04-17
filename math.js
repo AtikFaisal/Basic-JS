f6bab559dfbcb813a158b83041f2388fa0c6c1c8
Atik Faisal <atik.faisal111684@gmail.com>
2022-04-17 12:01:02 +0600

    math

--- math.js
+++ math.js
@@ -1,7 +1,23 @@
-let firstName = prompt ("What is your First Name")
-let partnerName = prompt ("What is your Partner Name")
+// let firstName = prompt ("What is your First Name")
+// let partnerName = prompt ("What is your Partner Name")
 
-let number = Math.random() * 100;
-number = Math.floor(number);
+// let number = Math.random() * 100;
+// number = Math.floor(number);
 
-console.log(firstName + " and " + partnerName + "'s love percentage is; " + number + "%")
+// console.log(firstName + " and " + partnerName + "'s love percentage is; " + number + "%")
+
+// console.log(Math.E)
+// console.log(Math.PI)
+
+// var n = 4.346
+// console.log(Math.abs(n))
+// console.log(Math.floor(n))
+// console.log(Math.ceil(n))
+// console.log(Math.round(n))
+// console.log(Math.max(200, 300, 600, 250))
+// console.log(Math.min(200, 300, 600, 250))
+// console.log(Math.pow(2, 3))
+// console.log(Math.pow(4, 3))
+// console.log(Math.sqrt(16))
+// console.log(Math.sqrt(81))
+console.log(Math.round(Math.random() * 50 + 1))
---
+++ operator.js
@@ -0,0 +1,21 @@
+//Arithmetic Operator + - * / % ++ --
+
+// var a = 13;
+// var b = 4;
+
+// console.log(a % b);
+
+//Assignment Operators
+// 
+
+//Comparison Operators
+
+var a = 10;
+var b = 20;
+
+console.log(a == b)
+console.log(a != b)
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
