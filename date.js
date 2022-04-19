58c1868d52fc113cb4790d3a7acd0004041570da
Atik Faisal <atik.faisal111684@gmail.com>
2022-04-19 10:17:19 +0600

    date

---
+++ date.js
@@ -0,0 +1,11 @@
+var date = new date();
+console.log(date.toDateString())
+console.log(date.toTimestring())
+console.log(date.toLocalstring())
+
+console.log(date.getFullYear())
+console.log(date.getMonth())
+console.log(date.getDate())
+console.log(date.getHours())
+console.log(date.getMinites())
+console.log(date.getSeconds())
