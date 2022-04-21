408215293c59a516a51b02e94bf30f8f68d7f697
Atik Faisal <atik.faisal111684@gmail.com>
2022-04-21 09:59:35 +0600

    if-condition

---
+++ if-condition.js
@@ -0,0 +1,20 @@
+var a = 10
+var b = 40
+
+if (a > b) {
+    console.log(a + ' is greater then ' + b)
+}
+
+if (a < b) {
+    console.log(b + ' is greater than ' + a)
+}
+
+var n = 5
+
+if (n % 2 === 0) {
+    console.log(n + ' is Even Numbers')
+}
+
+if (n % 2 === 1) {
+    console.log(n + ' is Odd Numbers')
+}
