d928f42f469a46900292f7b29d108cdc25777859
Atik Faisal <atik.faisal111684@gmail.com>
2022-04-28 10:42:15 +0600

    escape-notation

---
+++ escape-notation.js
@@ -0,0 +1,26 @@
+var str = 'This is a string'
+console.log(str)
+
+var str = 'This is a \'string\''
+console.log(str)
+
+var str = 'This is a \nstring'
+console.log(str)
+
+var str = 'This is a //string'
+console.log(str)
+
+var str = 'This is a \tstring'
+console.log(str)
+
+var str = 'This is a \vstring'
+console.log(str)
+
+var str = 'This is a \bstring'
+console.log(str)
+
+var str = 'This is a \rstring'
+console.log(str)
+
+var str = 'This is a \fstring'
+console.log(str)
