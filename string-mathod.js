024efea751a3d02cdbaad65ee488cf29ca268104
Atik Faisal <atik.faisal111684@gmail.com>
2022-04-28 11:06:58 +0600

    string-mathod

---
+++ string-mathod.js
@@ -0,0 +1,13 @@
+var a = 'I am'
+var b = 'Md. Atik Faisal'
+var c = a.concat(' ', b)
+//var d = c.substr(8)
+// var d = c.substr(8, 5)
+// console.log(c.charAt(6))
+// console.log(c.startsWith('I'))
+// console.log(c.endsWith('l'))
+
+// console.log(a.toUpperCase())
+// console.log(b.toLowerCase())
+// console.log('      Md. Atik Faisal     '.trim())
+console.log(c.split(' '))
