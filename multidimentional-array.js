182cb672639e05c80e6150d9ffd5c323132d4819
Atik Faisal <atik.faisal111684@gmail.com>
2022-05-12 10:13:13 +0600

    multidimentional-array

---
+++ multidimentional-array.js
@@ -0,0 +1,11 @@
+var arr = [
+    [22, 43, 12, 15],
+    [49, 14, 84, 54],
+    [33, 54, 27, 41]
+]
+
+for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+        console.log('Element ' + i + ': ' + arr[i][j])
+    }
+}
